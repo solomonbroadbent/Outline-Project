0b22d46ef6a97074c38165f01fa8bc45d7de40b9
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2017-11-28 11:26:41 +1300

    Added routing for bills by their IDs

--- source/private/router/api/billsRouter.js
+++ source/private/router/api/billsRouter.js
@@ -3,5 +3,7 @@ import express from 'express';
 const billsRouter = express.Router();
 const allBillsRoute = billsRouter.route('/');
 allBillsRoute.all((request, response) => response.send(501));
+const specificBillsRoute = billsRouter.route('/:id');
+specificBillsRoute.get((request, response) => response.send(200, request.params.id));
 
 export default billsRouter;
