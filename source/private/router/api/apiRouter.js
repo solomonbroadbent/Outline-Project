967b66553a88094822321e199b907bdecfb1fb2b
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2017-11-28 11:08:10 +1300

    Modified root API call to send HTTP codes

--- source/private/router/api/apiRouter.js
+++ source/private/router/api/apiRouter.js
@@ -4,14 +4,14 @@ import billsRouter from './billsRouter';
 const apiRouter = express.Router();
 const rootRoute = apiRouter.route('/');
 rootRoute.get((request, response) => {
-	response.send('This is the API');
+	response.send(200, 'This is the API');
 });
 rootRoute.all(sendResponseForUnsupportedOperation);
 apiRouter.use('/bills', billsRouter);
 
 
 function sendResponseForUnsupportedOperation(request, response) {
-	response.send('Unfortunately that operation is unsupported');
+	response.send(501, 'Unfortunately that operation is unsupported');
 }
 
 export default apiRouter;
